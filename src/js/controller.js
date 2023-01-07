f2136e08e518dbdb43fc5b725db7607b3c45592c
stephen <thaganasteve51@gmail.com>
2023-01-07 16:19:40 +0300

    initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,37 @@
+import * as model from './model.js';
+import recipeView from './views/recipeViews.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpiner();
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+
+    // 2.rendering the recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
